0d29058191c8e915084fa3a4f8ddbec912bd958a
Drake Scott <dascott0204@gmail.com>
2017-05-11 04:18:03 -0500

    Action Parameters in ASP. NET MVC

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,14 +13,34 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek!"};
-                ;
-//            return View(movie);
+                
+            return View(movie);
 //            return new ViewResult();
 //            return Content("Hello World");
 //            return HttpNotFound();
 //            return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortby = "name"});
+//            return RedirectToAction("Index", "Home", new { page = 1, sortby = "name"});
 
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
     }
