31f268ba63bd69e6a720ca3ecc9c10ea792f9b70
Drake Scott <dascott0204@gmail.com>
2017-05-11 04:27:15 -0500

    Convention Based Routing in ASP.NET MVC

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -23,24 +23,29 @@ namespace Vidly.Controllers
 
         }
 
-        public ActionResult Edit(int id)
+//        public ActionResult Edit(int id)
+//        {
+//            return Content("id=" + id);
+//        }
+
+//        public ActionResult Index(int? pageIndex, string sortBy)
+//        {
+//            if (!pageIndex.HasValue)
+//            {
+//                pageIndex = 1;
+//            }
+//
+//            if (String.IsNullOrWhiteSpace(sortBy))
+//            {
+//                sortBy = "Name";
+//            }
+//
+//            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+//        }
+
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return Content(year + "/" + month);
         }
 
     }
